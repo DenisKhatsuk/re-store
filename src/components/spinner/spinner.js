44c61bfdef28338a3c079e3369cf50ecf75098e6
Dzianis Khatsuk <dennis.hatsuk@gmail.com>
2021-03-27 19:56:50 +0300

    feat: create basic components

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-spin-h7bh05fqjok">
+      <div class="ldio-mt1rj50v0d9">
+        <div><div></div></div>
+        <div><div></div></div>
+        <div><div></div></div>
+        <div><div></div></div>
+        <div><div></div></div>
+        <div><div></div></div>
+        <div><div></div></div>
+        <div><div></div></div>
+        <div><div></div></div>
+        <div><div></div></div>
+        <div><div></div></div>
+        <div><div></div></div>
+        <div><div></div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
