c2b3da08f017a38356545b7b163ddf7c33c2454e
Dzianis Khatsuk <dennis.hatsuk@gmail.com>
2021-03-28 16:14:12 +0300

    feat: add book-list and book-list-item components

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import BookList from '../book-list';
 
 const HomePage = () => {
+
+  const books = [
+    {
+      id: 1,
+      title: 'JavaScript and jQuery: Interactive Front-End Web Development',
+      author: 'Jon Duckett',
+    },
+    {
+      id: 2,
+      title: 'You Don\'t Know JS: Scope & Closures',
+      author: 'Kyle Simpson',
+    },
+  ];
   return (
     <div className = "home-page">
-      Home Page
+      <BookList books = { books } />
     </div>
   );
 };
