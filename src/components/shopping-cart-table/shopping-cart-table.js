33dacda4953bc5b63693d26fdd9786350f024005
Dzianis Khatsuk <dennis.hatsuk@gmail.com>
2021-03-29 15:24:52 +0300

    feat: add action buttons to cart table

--- src/components/shopping-cart-table/shopping-cart-table.js
+++ src/components/shopping-cart-table/shopping-cart-table.js
@@ -20,7 +20,17 @@ const ShoppingCartTable = () => {
             <td>JavaScript and jQuery: Interactive Front-End Web Development</td>
             <td>1</td>
             <td>$16.51</td>
-            <td>Action</td>
+            <td>
+            <button className = "btn btn-outline-danger btn-sm float-right">
+                <i className = "fa fa-trash-o" />
+              </button>
+              <button className = "btn btn-outline-success btn-sm float-right">
+                <i className = "fa fa-plus-circle" />
+              </button>
+              <button className = "btn btn-outline-warning btn-sm float-right">
+                <i className = "fa fa-minus-circle" />
+              </button>
+            </td>
           </tr>
         </tbody>
       </table>
